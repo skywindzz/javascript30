634f4576f16e71855c4139c0b62d0478fcbe988b
skywindzz <skywindzz@yahoo.com>
2017-03-24 11:50:20 -0700

    3/24

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,14 +24,48 @@ function paintToCanvas(){
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        //take the pixel out
+        let pixels = ctx.getImageData(0,0, width,height);
+        //mess with them
+        //pixels = redEffect(pixels);
+        pixels = rgbsplit(pixels);
+        ctx.globalAlpha = 0.1;
+        //put them back
+        ctx.putImageData(pixels, 0, 0)
     }, 16);
 }
 
 function takePhoto() {
+    //played the sond
     snap.currenTime = 0;
     snap.play();
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
+    strip.insertBefore(link, strip.firstChild);
+    console.log(data);
 }
 
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; //r
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //b
+    }
+    return pixels;
+}
+
+function rgbsplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //r
+        pixels.data[i + 100] = pixels.data[i + 1]; //g
+        pixels.data[i - 150] = pixels.data[i + 2]; //b
+    }
+    return pixels; 
+}
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
 
